825a444d4a68ff19e66e0532485dd143f8e384a7
Leo <leonel.m.cata03@gmail.com>
2026-04-05 22:34:07 +0100

    tp2 ej2

--- TP2/Ej2/Racional.java
+++ TP2/Ej2/Racional.java
@@ -5,10 +5,16 @@ public class Racional {
 //variable del numero racional
     private int numerador, denominador;
 
+    // static hace que la variable sea de la clase y no de un objecto
+    public static int contadorObjetos;
+
 //constructor para asignal los valores
     public Racional(int numerador, int denominador) {
         this.numerador = numerador;
         this.denominador = denominador;
+
+        //contador de cuantes veces se crero un objecto
+        contadorObjetos++;
     }
 
     //metodo para sumar racionales
@@ -17,7 +23,12 @@ public class Racional {
         int nuevoNumerador = this.numerador * arg.denominador + this.denominador * arg.numerador;
         int nuevoDenominador = this.denominador * arg.denominador;
 
-        return new Racional(nuevoNumerador, nuevoDenominador);
+        // guarda el resultado en un nuevo Racional
+        Racional resultado = new Racional(nuevoNumerador, nuevoDenominador);
+
+        // simplifica el nuevo racional antes de devolverlo
+        resultado.simplificar();
+        return resultado;
     }
 
     //metodo para restar racionales
@@ -26,7 +37,12 @@ public class Racional {
         int nuevoNumerador = this.numerador * arg.denominador - this.denominador * arg.numerador;
         int nuevoDenominador = this.denominador * arg.denominador;
 
-        return new Racional(nuevoNumerador, nuevoDenominador);
+        // guarda el resultado en un nuevo Racional
+        Racional resultado = new Racional(nuevoNumerador, nuevoDenominador);
+
+        // simplifica el nuevo racional antes de devolverlo
+        resultado.simplificar();
+        return resultado;
     }
 
     //metodo para multiplicar racionales
@@ -35,7 +51,12 @@ public class Racional {
         int nuevoNumerador = this.numerador * arg.numerador;
         int nuevoDenominador = this.denominador * arg.denominador;
 
-        return new Racional(nuevoNumerador, nuevoDenominador);
+        // guarda el resultado en un nuevo Racional
+        Racional resultado = new Racional(nuevoNumerador, nuevoDenominador);
+
+        // simplifica el nuevo racional antes de devolverlo
+        resultado.simplificar();
+        return resultado;
     }
 
     //metodo para dividir racionales
@@ -44,7 +65,12 @@ public class Racional {
         int nuevoNumerador = this.numerador * arg.denominador;
         int nuevoDenominador = this.denominador * arg.numerador;
 
-        return new Racional(nuevoNumerador, nuevoDenominador);
+        // guarda el resultado en un nuevo Racional
+        Racional resultado = new Racional(nuevoNumerador, nuevoDenominador);
+
+        // simplifica el nuevo racional antes de devolverlo
+        resultado.simplificar();
+        return resultado;
     }
 
     //metodo para potencias racionales
@@ -53,13 +79,42 @@ public class Racional {
         int nuevoNumerador = Math.powExact(this.numerador, exponente);
         int nuevoDenominador = Math.powExact(this.denominador, exponente);
 
-        return new Racional(nuevoNumerador, nuevoDenominador);
+        // guarda el resultado en un nuevo Racional
+        Racional resultado = new Racional(nuevoNumerador, nuevoDenominador);
+
+        // simplifica el nuevo racional antes de devolverlo
+        resultado.simplificar();
+        return resultado;
+    }
+
+    // funcion para buscar el maximo comun divisor 
+    // repitiendo el modulo del numerador por el denominador
+    // hasta que el resto sea 0, osea n/d= resto 0
+    private int MCD(int numerador, int denominador) {
+        // mientras que el denominador sea distinto de 0
+        // guarda temp el denominador
+        // saca el mod del num y el deno
+        // asigna el numerador el deno temp
+        while (denominador != 0) {
+            int denominadorTemp = denominador;
+            denominador = numerador % denominador;
+            numerador = denominadorTemp;
+        }
+        return numerador;
+    }
+
+// funcion para simplificar la funcion utilizando el divisor encontrado en la funcion de simplificar
+    public void simplificar() {
+        int divisor = MCD(this.numerador, this.denominador);
+        this.numerador /= divisor;
+        this.denominador /= divisor;
     }
 
     // override indica al compilador que se sobreescribe el metodo estandar de impresion
     // entonces utiliza esta manera cuando se use un println
     @Override
     public String toString() {
-        return this.numerador + "/" + this.denominador;
+        return (this.denominador == 0) ? this.numerador + "" : this.numerador + "/" + this.denominador;
+
     }
 }
--- TP2/Ej2/RacionalTest.java
+++ TP2/Ej2/RacionalTest.java
@@ -4,5 +4,18 @@ public class RacionalTest {
 
     public static void main(String[] args) {
 
+        Racional r1 = new Racional(1, 2);
+        Racional r2 = new Racional(1, 2);
+
+        System.out.print("Suma ");
+        System.out.println(r1.suma(r2));
+        System.out.print("Resta ");
+        System.out.println(r1.resta(r2));
+        System.out.print("multiplicacion ");
+        System.out.println(r1.multiplicacion(r2));
+        System.out.print("cociente ");
+        System.out.println(r1.potencia(2));
+        System.out.print("division ");
+        System.out.println(r1.division(r2));
     }
 }
